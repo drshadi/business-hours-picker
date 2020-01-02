b2b1062083084fd2f3f80c071bcceb116d5b343f
drshadi <46031429+drshadi@users.noreply.github.com>
2020-01-02 14:09:44 +0200

    Update BusinessHoursWeekPicker.java

--- bhlib/src/main/java/com/bashizip/bhlib/BusinessHoursWeekPicker.java
+++ bhlib/src/main/java/com/bashizip/bhlib/BusinessHoursWeekPicker.java
@@ -39,40 +39,41 @@ public class BusinessHoursWeekPicker extends LinearLayout {
 
         this.setOrientation(VERTICAL);
 
-        BusinessHoursPicker monday = new BusinessHoursPicker(this.getContext());
-        monday.setDayOfWeek(context.getString(R.string.bhv_monday));
-        businessHoursPickerList.add(monday);
-        this.addView(monday, 1);
-
-        BusinessHoursPicker tuesday = new BusinessHoursPicker(this.getContext());
-        tuesday.setDayOfWeek(context.getString(R.string.bhv_tuesday));
-        businessHoursPickerList.add(tuesday);
-        this.addView(tuesday, 2);
-
-        BusinessHoursPicker wednesday = new BusinessHoursPicker(this.getContext());
-        wednesday.setDayOfWeek(context.getString(R.string.bhv_wednesday));
-        businessHoursPickerList.add(wednesday);
-        this.addView(wednesday, 3);
-
-        BusinessHoursPicker thursday = new BusinessHoursPicker(this.getContext());
-        thursday.setDayOfWeek(context.getString(R.string.bhv_thursday));
-        businessHoursPickerList.add(thursday);
-        this.addView(thursday, 4);
-
-        BusinessHoursPicker friday = new BusinessHoursPicker(this.getContext());
-        friday.setDayOfWeek(context.getString(R.string.bhv_friday));
-        businessHoursPickerList.add(friday);
-        this.addView(friday, 5);
 
         BusinessHoursPicker saturday = new BusinessHoursPicker(this.getContext());
         saturday.setDayOfWeek(context.getString(R.string.bhv_saturday));
         businessHoursPickerList.add(saturday);
-        this.addView(saturday, 6);
+        this.addView(saturday, 1);
 
         BusinessHoursPicker sunday = new BusinessHoursPicker(this.getContext());
         sunday.setDayOfWeek(context.getString(R.string.bhv_sunday));
         businessHoursPickerList.add(sunday);
-        this.addView(sunday, 7);
+        this.addView(sunday, 2);
+        
+        BusinessHoursPicker monday = new BusinessHoursPicker(this.getContext());
+        monday.setDayOfWeek(context.getString(R.string.bhv_monday));
+        businessHoursPickerList.add(monday);
+        this.addView(monday, 3);
+
+        BusinessHoursPicker tuesday = new BusinessHoursPicker(this.getContext());
+        tuesday.setDayOfWeek(context.getString(R.string.bhv_tuesday));
+        businessHoursPickerList.add(tuesday);
+        this.addView(tuesday, 4);
+
+        BusinessHoursPicker wednesday = new BusinessHoursPicker(this.getContext());
+        wednesday.setDayOfWeek(context.getString(R.string.bhv_wednesday));
+        businessHoursPickerList.add(wednesday);
+        this.addView(wednesday, 5);
+
+        BusinessHoursPicker thursday = new BusinessHoursPicker(this.getContext());
+        thursday.setDayOfWeek(context.getString(R.string.bhv_thursday));
+        businessHoursPickerList.add(thursday);
+        this.addView(thursday, 6);
+
+        BusinessHoursPicker friday = new BusinessHoursPicker(this.getContext());
+        friday.setDayOfWeek(context.getString(R.string.bhv_friday));
+        businessHoursPickerList.add(friday);
+        this.addView(friday, 7);
 
 
     }
